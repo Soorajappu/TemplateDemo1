01eb709b2dc3b46343e51c1fa279e619f1439c28
unknown <soorajkg>
2024-07-24 14:55:13 +0530

    Login and Signup Page Commit

--- templates/src/components/Navbar.jsx
+++ templates/src/components/Navbar.jsx
@@ -18,17 +18,17 @@ const Navbar = () => {
                     <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
                         <Link aria-current="page"
                             className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                            href="/">Home</Link>
+                            to={"/TemplateDemo1/"}>Home</Link>
                         <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                            href="/About">About</Link>
+                            to={"/TemplateDemo1/About"}>About</Link>
                         <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                            href="/Contact">Contact</Link>
+                            to={"/TemplateDemo1/Contact"}>Contact</Link>
                     </div>
                     <div className="flex items-center justify-end gap-3">
                         <Link className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                            href="/Signup">Sign up</Link>
+                            to={"/Signup"}>Sign up</Link>
                         <Link className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                            href="/Login">Login</Link>
+                            to={"/Login"}>Login</Link>
                     </div>
                 </div>
             </div>
